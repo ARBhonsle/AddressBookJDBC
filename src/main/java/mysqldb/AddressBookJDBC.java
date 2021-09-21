a7a609a1737207e08bef9ab69bb16420cd344edb
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-21 21:15:32 +0530

    Added: Connection to database

--- src/main/java/mysqldb/AddressBookJDBC.java
+++ src/main/java/mysqldb/AddressBookJDBC.java
@@ -1,5 +1,40 @@
 package mysqldb;
 
-public class AddressBookJDBC {
+import java.sql.*;
 
+public class AddressBookJDBC {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/address_book_service";
+        String user = "root";
+        String password = "root@1234";
+        Connection con = null;
+        try{
+            // connect with database
+            con = DriverManager.getConnection(url,user,password);
+            con.setAutoCommit(false);
+            // query entries from database table
+            Statement stmt = con.createStatement();
+
+            ResultSet resultSet = stmt.executeQuery("select * from address_book;");
+            ResultSetMetaData metaData = resultSet.getMetaData();
+
+            //  show/display entries from table
+            while (resultSet.next()){
+                for(int i=0;i<metaData.getColumnCount();i++){
+                    if(i==0){
+                        System.out.print(metaData.getColumnLabel(i+1)+"\t");
+                    }else{
+                        System.out.print(resultSet.getString(i)+"\t");
+                    }
+                }
+                System.out.println();
+            }
+            con.commit();
+        } catch (SQLException sqlException){
+            con.rollback();
+            sqlException.printStackTrace();
+        } finally {
+            con.close();
+        }
+    }
 }
